37955303e1fd99c40d1cd4a0bacd43d4305aec61
Emilio Peláez <me@epelaez.net>
2024-11-15 01:21:28 +0100

    Project Setup

---
+++ Project/MIDIVolumeControl/MIDIVolumeControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIDIVolumeControlApp.swift
+//  MIDIVolumeControl
+//
+//  Created by Emilio Peláez on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MIDIVolumeControlApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
