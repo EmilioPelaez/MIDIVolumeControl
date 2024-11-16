ca65b5829d9d893e45b32ae69fd29da3643a08ca
Emilio Peláez <me@epelaez.net>
2024-11-16 19:01:02 +0100

    Volume Controller Interface

--- Project/MIDIVolumeControl/Logic/VolumeController.swift
+++ Project/MIDIVolumeControl/Logic/VolumeController.swift
@@ -16,12 +16,31 @@ class VolumeController {
 	var configuration: Configuration {
 		didSet {
 			configuration.persist(to: defaults)
+			configureStream()
 		}
 	}
 	
 	init(defaults: UserDefaults = .standard) {
 		self.defaults = defaults
 		self.configuration = .init(from: defaults)
+		
+		configureStream()
 	}
 	
+	func configureStream() {
+		if configuration.enabled {
+			//	Enable Stream
+		} else {
+			//	Disable Stream
+		}
+	}
+	
+	func receiveValue(_ value: Int) {
+		let newVolume = convertValue(value)
+		Sound.output.volume = newVolume
+	}
+	
+	func convertValue(_ value: Int) -> Float {
+		fatalError()
+	}
 }
